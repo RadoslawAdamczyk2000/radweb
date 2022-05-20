248b041a301e8905cbfcbffb3c7f4d95ec246bd6
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-05-20 10:56:10 +0200

    update hero on about subpages

--- pages/o-mnie/certyfikaty-i-kursy.tsx
+++ pages/o-mnie/certyfikaty-i-kursy.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
+import Hero from "../../components/All/Hero/Hero";
 import Cards from "../../components/Page/About/Certificates/Card/Cards";
-import Hero from "../../components/Page/All/Hero/Hero";
 import Layout from "../../schemas/Layout";
 const Page:NextPage = () => {
     return(
@@ -8,7 +8,8 @@ const Page:NextPage = () => {
             <Hero
                 content="Jednym z dowodów na rozwój jest nauka poprzez kursy oraz zdobywanie branżowych certyfikatów. W branży IT tematycznych kursów - często bezpłatnych - nie brakuje.Jednym z dowodów na rozwój jest nauka poprzez kursy oraz zdobywanie branżowych certyfikatów. W branży IT tematycznych kursów - często bezpłatnych - nie brakuje.Jednym z dowodów na rozwój jest nauka poprzez kursy oraz zdobywanie branżowych certyfikatów. W branży IT tematycznych kursów - często bezpłatnych - nie brakuje."
                 image="https://images.unsplash.com/photo-1627556704283-452301a45fd0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                isImage={true}
+                withImage={true}
+                isVertical={true}
                 title="Certyfikaty i kursy"
             />
             <main>
--- pages/o-mnie/kariera.tsx
+++ pages/o-mnie/kariera.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
+import Hero from "../../components/All/Hero/Hero";
 import Cards from "../../components/Page/About/Career/Cards";
-import Hero from "../../components/Page/All/Hero/Hero";
 import Layout from "../../schemas/Layout";
 const Page:NextPage = () => {
     return(
@@ -8,7 +8,8 @@ const Page:NextPage = () => {
             <Hero
                 content="Jestem młodym programistą z codziennie rosnącym bagażem doświadczeń w świecie programowania i marketingu stron internetowych w wyszukiwarce Google. Podążam za aktualnymi trendami oraz czerpię wiedzę od renomowanych autorytetów w branży.Jestem młodym programistą z codziennie rosnącym bagażem doświadczeń w świecie programowania i marketingu stron internetowych w wyszukiwarce Google. Podążam za aktualnymi trendami oraz czerpię wiedzę od renomowanych autorytetów w branży.Jestem młodym programistą z codziennie rosnącym bagażem doświadczeń w świecie programowania i marketingu stron internetowych w wyszukiwarce Google. Podążam za aktualnymi trendami oraz czerpię wiedzę od renomowanych autorytetów w branży."
                 image="https://images.unsplash.com/photo-1507679799987-c73779587ccf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
-                isImage={true}
+                withImage={true}
+                isVertical={true}
                 title='Kariera'
             />
             <main>
