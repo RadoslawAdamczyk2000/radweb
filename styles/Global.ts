000c0a7d24cd85124501b35fb8f76d9ec60c96e4
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-06-02 20:04:12 +0200

    remove theme imports

--- styles/Global.ts
+++ styles/Global.ts
@@ -1,6 +1,5 @@
 import { createGlobalStyle} from "styled-components";
 export const Global = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Catamaran:wght@100;200;300;400;500;600;700;800;900&display=swap');
     * {
         box-sizing:border-box;
         margin:0;
@@ -13,17 +12,12 @@ export const Global = createGlobalStyle`
         position:absolute;
     }
     ::selection{
-        background-color:${({theme}) => theme.colors.quatenary};
-        color:${({theme}) => theme.colors.headings.title};
     }
     a{
         color:inherit;
         text-decoration:none;
     }
     body{
-        background-color:${({theme}) => theme.colors.background};
-        color:${({theme}) => theme.colors.color};
-        font-family: 'Catamaran', sans-serif;
         font-size:1.6rem;
         font-weight:400;
         transition:.35s linear background-color,.35s linear color;
@@ -34,34 +28,11 @@ export const Global = createGlobalStyle`
     html{
         font-size:62.5%;
     }
-    main{
-        .big{
-            width:99%;
-        }
-        .larger{
-            width:90%;
-        }
-        margin:4rem auto;
-        padding:1rem 0;
-        width:85%;
-        & > section{
-            margin:8rem auto;
-        }
-        &.densely > section {
-            margin:1rem auto;
-        }
-        @media only screen {
-            @media (max-width:1500px){
-                width:90%;
-            }
-        }
-    }
     button,
     input,
     textarea{
         background:none;
         border:none;
-        font-family: 'Catamaran', sans-serif;
         outline:none;
     }
     li,
