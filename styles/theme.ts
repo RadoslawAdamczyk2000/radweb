9f88210f63fa589e6d7bd0727d49223ce024ec34
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-06-02 20:55:37 +0200

    create theme

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,12 +1,40 @@
-export const lightTheme = {
-    colors:{
-
-    },
-    fonts:{
-        family:{
-            code:"'Roboto Mono', monospace",
-            heading:"'X-Heighting','Poppins', sans-serif",
-            regular:"'Poppins', sans-serif",
-        }
+import { DefaultTheme } from "styled-components"
+const fonts = {
+    family:{
+        code:"'Roboto Mono', monospace",
+        heading:"'X-Heighting','Poppins', sans-serif",
+        regular:"'Poppins', sans-serif",
     }
+}
+export const darkTheme:DefaultTheme = {
+    colors:{
+        background:'#212121',
+        color:'#f5f5f5',
+        gray:'#d6d6d6',
+        primary:'#c3dcf1',
+        secondary:'#d63c75',
+        tertiary:'#cf3a71',
+        quatenary:'#e60457',
+        code:{
+            background:'#333333',
+            color:'#1DC6A4'
+        }
+    },
+    fonts:{...fonts}
+}
+export const lightTheme:DefaultTheme = {
+    colors:{
+        background:'#fdfdfd',
+        color:'#0a0a0a',
+        gray:'#3d3d3d',
+        primary:'#00C2B2',
+        secondary:'#740CAC',
+        tertiary:'#DA00A9',
+        quatenary:'#FF3569',
+        code:{
+            background:'#00291D',
+            color:'#F9F871'
+        }
+    },
+    fonts:{...fonts}
 }
---
+++ typings/styled.d.ts
@@ -0,0 +1,25 @@
+import 'styled-components';
+declare module 'styled-components' {
+    export interface DefaultTheme {
+        colors:{
+            background:string;
+            color:string;
+            gray:string;
+            primary:string;
+            secondary:string;
+            tertiary:string;
+            quatenary:string;
+            code:{
+                background:string;
+                color:string;
+            }
+        },
+        fonts:{
+            family:{
+                code:string;
+                heading:string;
+                regular:string;
+            }
+        }
+    }
+}
