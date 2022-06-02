2f28439ecd520f352561ed36b1d0df0bb5c62b09
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-06-02 18:43:36 +0200

    vanish pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,9 @@
 import { NextPage } from "next";
-import About from "../components/Page/Home/About/About";
-import Blog from "../components/Page/Home/Blog/Blog";
-import Hero from "../components/Page/Home/Hero/Hero";
-import Movie from "../components/Page/Home/Movie/Movie";
-import Offer from "../components/Page/Home/Offer/Offer";
-import Projects from "../components/Page/Home/Projects/Projects";
-import Steps from "../components/Page/Home/Steps/Steps";
-import { home } from "../content/home";
-import Layout from "../schemas/Layout";
 const Page:NextPage = () => {
-  const {description,image,title} = home.seo;
   return(
-    <Layout
-      description={description}
-      image={image}
-      title={title}
-    >
-      <Hero/>
-      <main>
-        <About/>
-        <Blog/>
-        <Movie/>
-        <Offer/>
-        <Projects/>
-        <Steps/>
-      </main>
-    </Layout>
+    <>
+      asd
+    </>
   )
 }
 export default Page;
