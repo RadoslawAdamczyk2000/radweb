b9b4d06233761e72ad78a3bb8e77450ed6981e70
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-06-06 14:54:11 +0200

    add changes to menu button

--- components/Layout/Navigation/Options/MenuButton/MenuButton.tsx
+++ components/Layout/Navigation/Options/MenuButton/MenuButton.tsx
@@ -3,34 +3,15 @@ import { Context } from "../../../../../context/MenuContext";
 import { MenuButtonWrapper } from "./styles";
 const MenuButton = () => {
     const { isActive, handle } = useContext(Context);
-    const [isMobile,setMobile] = useState(false);
-    const handleDevice = async () => {
-        if(typeof window !== undefined){
-            if(window.innerWidth > 900){
-                setMobile(false);
-            }else{
-                setMobile(true);
-            }
-        }
-    }
-    useEffect(() => {
-        handleDevice();
-    },[])
     console.log(isActive);
     return(
-        <>
-            {
-                isMobile &&
-                <MenuButtonWrapper 
-                    isActive={isActive} 
-                    onClick={handle}
-                >
-                    <span/>
-                    <span/>
-                </MenuButtonWrapper>
-            }
-            
-        </>
+        <MenuButtonWrapper 
+            isActive={isActive} 
+            onClick={handle}
+        >
+            <span/>
+            <span/>
+        </MenuButtonWrapper>
     )
 }
 export default MenuButton;
