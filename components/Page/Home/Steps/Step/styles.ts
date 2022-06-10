2a2fae3c67e38d59be918dafe3df6269b24c34fa
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-06-10 19:46:46 +0200

    finish steps

--- components/Page/Home/Steps/Step/styles.ts
+++ components/Page/Home/Steps/Step/styles.ts
@@ -1,75 +1,103 @@
 import styled from 'styled-components';
-const gridWidth = '100% - 8rem';
 export const StepWrapper = styled.li`
     align-items:start;
-    display:grid;
-    justify-items:center;
+    /* background:darkblue; */
+    display:flex;
+    flex-wrap:nowrap;
     margin:auto;
-    width:75%;
-    & > * {
-        align-items:center;
-        display:flex;
-        flex-flow:column;
-        height:100%;
-        justify-content:center;
+    width:65%;
+    @media only screen {
+        @media (max-width:1050px){
+            width:85%;
+        }
+        @media (max-width:800px){
+            width:95%;
+        }
+        @media (max-width:650px){
+            flex-direction:column !important;
+            justify-content:center;
+        }
     }
-    & > div{
-        height:7rem;
-        width:7rem;
+    & > * {
+        flex-shrink:0;
+    }
+    & > div {
+        /* background-color:red; */
+        
+        flex:6rem;
+        height:6rem;
+        width:6rem;
+        @media only screen{
+            @media (max-width:650px){
+            }
+        }
         & > span{
             align-items:center;
             color:${({theme}) => theme.colors.primary};
             display:flex;
             font-family:${({theme}) => theme.fonts.family.heading};
-            font-size:5rem;
+            font-size:4rem;
             height:100%;
             justify-content:center;
             width:100%;
         }
     }
     & > section{
-        width:100%;
-        & > * {
+        /* background:darkcyan;. */
+        display:flex;
+        flex:calc(100% - 6rem);
+        flex-flow:column nowrap;
+        justify-content:center;
+        & > * { 
             width:100%;
         }
-        & > h3 {
+        & > h3{
             color:${({theme}) => theme.colors.color};
-            font-size:2.85rem;
+            font-size:2.8rem;
             font-weight:500;
-            padding:.5rem .85rem;
+            @media only screen{
+                @media (max-width:700px){
+                    font-size:2.45rem;
+                }
+            }
         }
         & > p {
             color:${({theme}) => theme.colors.gray};
             font-size:1.7rem;
-            line-height:calc(1.7rem * 2);
+            line-height:calc(1.7rem * 1.85);
             opacity:.7;
-            padding:.55rem;
-        }
-
-    }
-    &:nth-of-type(odd){
-        background:blue;
-        grid-template-columns:${gridWidth} 7rem;
-        & > div{
-            grid-column:2;
-            background:red;
-        }
-        & > section{
-            background:green;
-            grid-column:1;
-            text-align:end;
+            @media only screen{
+                @media (max-width:700px){
+                    font-size:1.65rem;
+                    line-height:calc(1.65rem * 1.75);
+                }
+            }
         }
     }
     &:nth-of-type(even){
+        flex-direction:row;
         margin-top:15rem;
-        grid-template-columns:7rem ${gridWidth};
-        & > div{
-            grid-column:1;
-        }
         & > section{
-            grid-column:2;
+            text-align:end;
+        }
+        @media only screen{
+            @media (max-width:650px){
+                align-items:flex-end;
+            }
+            @media (max-width:540px){
+                margin-top:0rem;
+            }
+        }
+    }
+    &:nth-of-type(odd){
+        flex-direction:row-reverse;
+        & > section{
             text-align:start;
-            
+        }
+        @media only screen{
+            @media (max-width:650px){
+                align-items:flex-start;
+            }
         }
     }
 `
--- components/Page/Home/Steps/Steps/styles.ts
+++ components/Page/Home/Steps/Steps/styles.ts
@@ -1,9 +1,16 @@
 import styled from 'styled-components';
 export const StepsWrapper = styled.div`
-
+    margin:5rem 0;
 
     & > ul{
         display:grid;
+        grid-gap:.5rem .75rem;
         grid-template-columns:repeat(2,1fr);
+        padding:2rem 3rem;
+        @media only screen{
+            @media (max-width:540px){
+                grid-template-columns:repeat(1,1fr);
+            }
+        }
     }
 `
