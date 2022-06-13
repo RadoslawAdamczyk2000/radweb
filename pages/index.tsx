e5ac2621e066f5dde05bfeb03b2da61df14b39f6
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-06-13 16:49:05 +0200

    add layout to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import { NextPage } from "next";
 import { menu } from "../data/menu";
+import Layout from "../schemas/Layout";
 const Page:NextPage = () => {
   console.log(menu);
   return(
-    <>
+    <Layout>
       <>
         <main>
         </main>
       </>
-    </>
+    </Layout>
   )
 }
 export default Page;
